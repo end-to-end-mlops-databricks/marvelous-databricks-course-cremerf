58729939aa9f158b15bff24efb9d159341943fac
cremerf <cremerf>
2024-11-04 15:10:22 +0000

    log & register custom model 50%

--- notebooks/03.log_and_register_model.py
+++ notebooks/03.log_and_register_model.py
@@ -1,5 +1,6 @@
 # Databricks notebook source
 # Databricks notebook source
+!pip install python-dotenv
 from packages.config import ProjectConfig
 from packages.paths import AllPaths
 from pyspark.sql import SparkSession
@@ -12,74 +13,6 @@ import mlflow
 from mlflow.models import infer_signature
 import os
 from pathlib import Path
-
-# COMMAND ----------
-
-from pathlib import Path
-import yaml
-
-class AllPaths:
-    def __init__(self):
-        # Assuming your base directory is correctly set up
-        self.BASE_DIR = Path("/Workspace/Users/cremerfederico29@gmail.com/marvelmlops-cremerf")
-        self.filename_config = self.BASE_DIR / 'project-config.yml'
-        self.config = self.get_config_file()
-        # Update the paths below as necessary
-        self.cremerf_catalog = f'/Volumes/{self.config["catalog_name"]}/{self.config["schema_name"]}/'
-        self.data_volume = f"{self.cremerf_catalog}" + "data/"
-
-    def get_config_file(self):
-        # Load configuration
-        try:
-            with open(self.filename_config, "r") as file:
-                config = yaml.safe_load(file)
-            return config
-        except FileNotFoundError as e:
-            print(f"Error: {e}")
-            return None
-
-
-
-
-# COMMAND ----------
-
-import yaml
-from pathlib import Path
-import os
-
-class AllPaths:
-    def __init__(self) -> None:
-        if self.is_databricks():
-            # Running in Databricks
-            self.filename_config = Path('/Workspace/Users/cremerfederico29@gmail.com/marvelmlops-cremerf/project-config.yml')
-        else:
-            # Running in VSCode or other local environment
-            try:
-                self.BASE_DIR = Path(__file__).resolve().parent.parent.parent
-            except NameError:
-                # __file__ is not defined, use current working directory
-                self.BASE_DIR = Path.cwd()
-            self.filename_config = self.BASE_DIR / 'project-config.yml'
-        
-        self.config = self.get_config_file()
-        self.cremerf_catalog = f'/Volumes/{self.config["catalog_name"]}/{self.config["schema_name"]}/'
-        self.data_volume = f"{self.cremerf_catalog}" + "data/"
-    
-    def is_databricks(self):
-        # Check if the /databricks directory exists
-        return os.path.exists('/databricks')
-    
-    def get_config_file(self):
-        # Load configuration
-        try:
-            with open(self.filename_config, "r") as file:
-                config = yaml.safe_load(file)
-            return config
-        except FileNotFoundError:
-            print(f"Configuration file not found at {self.filename_config}")
-            raise
-
-
 
 # COMMAND ----------
 
--- notebooks/04.log_and_register_custom_model.py
+++ notebooks/04.log_and_register_custom_model.py
@@ -1,4 +1,8 @@
 # Databricks notebook source
+# MAGIC %pip install python-dotenv
+
+# COMMAND ----------
+
 # Databricks notebook source
 import mlflow
 import numpy as np
@@ -10,44 +14,6 @@ from packages.paths import AllPaths
 import json
 from mlflow import MlflowClient
 from mlflow.utils.environment import _mlflow_conda_env
-
-# COMMAND ----------
-
-from pathlib import Path
-import os
-import yaml
-
-class AllPaths:
-    def __init__(self) -> None:
-        if self.is_databricks():
-            # Running in Databricks
-            self.filename_config = Path('/Workspace/Users/cremerfederico29@gmail.com/marvelmlops-cremerf/project-config.yml')
-        else:
-            # Running in VSCode or other local environment
-            try:
-                self.BASE_DIR = Path(__file__).resolve().parent.parent.parent
-            except NameError:
-                # __file__ is not defined, use current working directory
-                self.BASE_DIR = Path.cwd()
-            self.filename_config = self.BASE_DIR / 'project-config.yml'
-        
-        self.config = self.get_config_file()
-        self.cremerf_catalog = f'/Volumes/{self.config["catalog_name"]}/{self.config["schema_name"]}/'
-        self.data_volume = f"{self.cremerf_catalog}" + "data/"
-    
-    def is_databricks(self):
-        # Check if the /databricks directory exists
-        return os.path.exists('/databricks')
-    
-    def get_config_file(self):
-        # Load configuration
-        try:
-            with open(self.filename_config, "r") as file:
-                config = yaml.safe_load(file)
-            return config
-        except FileNotFoundError:
-            print(f"Configuration file not found at {self.filename_config}")
-            raise
 
 # COMMAND ----------
 
@@ -89,4 +55,22 @@ model = mlflow.sklearn.load_model(f"runs:/{run_id}/lightgbm-pipeline-model")
 
 # COMMAND ----------
 
-model
+
+
+# COMMAND ----------
+
+train_set = spark.table(f"{catalog_name}.{schema_name}.train_set")
+test_set = spark.table(f"{catalog_name}.{schema_name}.test_set")
+
+X_train = train_set[num_features + cat_features].toPandas()
+y_train = train_set[[target]].toPandas()
+
+X_test = test_set[num_features + cat_features].toPandas()
+y_test = test_set[[target]].toPandas()
+
+# COMMAND ----------
+
+wrapped_model = HotelReservationseModelWrapper(model)  # we pass the loaded model to the wrapper
+example_input = X_test.iloc[0:1]  # Select the first row for prediction as example
+example_prediction = wrapped_model.predict(context=None, model_input=example_input)
+print("Example Prediction:", example_prediction)
