b6a7bc19f41c4aef90f5c0b85a5cd7cfbbfc0f34
cremerf <cremerf>
2024-11-20 21:28:02 +0000

    AB model serving testing 15%

--- notebooks/week3/04.AB_test_model_serving.py
+++ notebooks/week3/04.AB_test_model_serving.py
@@ -1,8 +1,45 @@
 # Databricks notebook source
+import time
 
+import mlflow
+import pandas as pd
+from databricks.sdk import WorkspaceClient
+from databricks.sdk.service.serving import EndpointCoreConfigInput, ServedEntityInput
+from lightgbm import LGBMRegressor
+from mlflow import MlflowClient
+from mlflow.models import infer_signature
+from pyspark.sql import SparkSession
+from sklearn.compose import ColumnTransformer
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
+import hashlib
+import requests
+
+from hotel_reservation.config import ProjectConfig
+from hotel_reservation.paths import AllPaths
+
+ALLPATHS = AllPaths()
 
 # COMMAND ----------
 
+# Set up MLflow for tracking and model registry
+mlflow.set_tracking_uri("databricks")
+mlflow.set_registry_uri("databricks-uc")
+
+# Initialize the MLflow client for model management
+client = MlflowClient()
+
+# Load configuration
+config = ProjectConfig.from_yaml(config_path=ALLPATHS.filename_config)
+
+# Extract key configuration details
+num_features = config.num_features
+cat_features = config.cat_features
+target = config.target
+catalog_name = config.catalog_name
+schema_name = config.schema_name
+ab_test_params = config.ab_test
 
 
 # COMMAND ----------
