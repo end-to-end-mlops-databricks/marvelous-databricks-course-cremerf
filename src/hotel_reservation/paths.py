e5c3d21ec37c1c4c28b17671d0c0d4d54d28871d
cremerf <cremerfederico29@gmail.com>
2024-11-20 15:39:59 -0300

    minor changes

--- src/hotel_reservation/paths.py
+++ src/hotel_reservation/paths.py
@@ -4,27 +4,29 @@ from pathlib import Path
 import yaml
 from dotenv import load_dotenv
 
+
 class AllPaths:
     def __init__(self) -> None:
-        load_dotenv()  # Load environment variables from .env file
+        load_dotenv()  # Load environment variables from .env file if present
 
-        self.base_dir = self.get_base_dir()
-        self.filename_config = self.base_dir / "project-config.yml"
+        if self.is_databricks():
+            # Running in Databricks
+            self.filename_config = Path(
+                os.environ.get(
+                    "DATABRICKS_CONFIG_PATH",
+                    "/Workspace/Users/cremerfederico29@gmail.com/marvelmlops-cremerf/project-config.yml",
+                )
+            )
+        else:
+            # Running in local environment
+            self.BASE_DIR = Path(__file__).resolve().parent.parent.parent
+            self.filename_config = self.BASE_DIR / "project-config.yml"
 
         self.config = self.get_config_file()
         self.cremerf_catalog = f'/Volumes/{self.config["catalog_name"]}/{self.config["schema_name"]}/'
         self.data_volume = f"{self.cremerf_catalog}data/"
 
-    def get_base_dir(self):
-        if self.is_databricks():
-            # In Databricks, the current working directory is accessible via os.getcwd()
-            return Path(os.getcwd())
-        else:
-            # In local environment, use the parent of the script's directory
-            return Path(__file__).resolve().parent.parent.parent
-
     def is_databricks(self):
-        # Use the IS_DATABRICKS environment variable from .env
         is_db_env = os.environ.get("IS_DATABRICKS")
         if is_db_env is not None:
             # Use the value from the environment variable
